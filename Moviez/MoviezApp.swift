c9a260c6d85df2bf30156e980aadb3a44067f0cf
Alaa AbuZarifa <alaa.s.zarifa@gmail.com>
2024-06-03 13:56:13 +0300

    Initial Commit

---
+++ Moviez/MoviezApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviezApp.swift
+//  Moviez
+//
+//  Created by Alaa Abuzarifa on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
