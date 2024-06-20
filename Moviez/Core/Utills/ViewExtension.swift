22ee1532c2757f17178aee42af14c66d47f75291
Alaa AbuZarifa <alaa.s.zarifa@gmail.com>
2024-06-20 14:47:37 +0300

    Refactoring Files

---
+++ Moviez/Core/Utills/ViewExtension.swift
@@ -0,0 +1,31 @@
+//
+//  ViewExtension.swift
+//  Moviez
+//
+//  Created by Alaa Abuzarifa on 20/06/2024.
+//
+
+import Foundation
+
+extension String {
+
+    func getYear() -> String? {
+    // Create a DateFormatter object specifying the input date format and locale
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX") // Set to US English (POSIX)
+
+    // Convert the string to a Date object (handle potential conversion error)
+    guard let date = dateFormatter.date(from: self) else {
+      return nil
+    }
+
+    // Create a calendar object
+    let calendar = Calendar.current
+
+    // Extract the year component from the date
+    let year = calendar.component(.year, from: date)
+
+        return String(year)
+  }
+}
